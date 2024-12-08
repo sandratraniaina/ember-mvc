e7443dac5f991900c5e5664bdef57ddf159f1fa9
sandratraniaina <sandratrarafa@gmail.com>
2024-12-08 17:19:55 +0300

    feat: Create getFieldExceptions method

--- src/mg/emberframework/util/validation/Validator.java
+++ src/mg/emberframework/util/validation/Validator.java
@@ -2,6 +2,8 @@ package mg.emberframework.util.validation;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 import mg.emberframework.manager.exception.ModelValidationException;
 import mg.emberframework.util.validation.validator.FieldValidator;
@@ -17,5 +19,24 @@ public class Validator {
                 validator.validate(value , annotation, field);
             }
         }
+    }
+
+    public static List<Exception> getFieldExceptions(String value, Field field) {
+        List<Exception> exceptions = new ArrayList<>();
+        Annotation[] annotations = field.getAnnotations();
+
+        for(Annotation annotation : annotations) {
+            FieldValidator validator = ValidatorRegistry.getValidator(annotation.annotationType());
+
+            if (validator != null) {
+                try {
+                    validator.validate(value , annotation, field);
+                } catch (Exception e) {
+                    exceptions.add(e);
+                }
+            }
+        }
+
+        return exceptions;
     }
 }
