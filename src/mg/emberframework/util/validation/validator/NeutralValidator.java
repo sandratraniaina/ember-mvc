cb4a2fc496ebc8e264470c2d0f1ddb83499f4ea7
sandratraniaina <sandratrarafa@gmail.com>
2024-12-13 09:50:48 +0300

    feat: Add NeutralValidator to check for field without validator annotation

---
+++ src/mg/emberframework/util/validation/validator/NeutralValidator.java
@@ -0,0 +1,15 @@
+package mg.emberframework.util.validation.validator;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+import mg.emberframework.manager.exception.ModelValidationException;
+
+public class NeutralValidator implements FieldValidator{
+
+    @Override
+    public void validate(String value, Annotation annotation, Field field) throws ModelValidationException {
+        // This method is empty because this validate field without validator annotation.
+    }
+    
+}
