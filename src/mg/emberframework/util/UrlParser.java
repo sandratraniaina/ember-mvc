831a0127f0e181f56b46f3338d9a8b962ad148be
sandratraniaina <sandratrarafa@gmail.com>
2025-02-17 21:27:00 +0300

    feat: Create getRoute method in UrlParser

---
+++ src/mg/emberframework/util/UrlParser.java
@@ -0,0 +1,31 @@
+package mg.emberframework.util;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class UrlParser {
+    private UrlParser() {
+    }
+
+    public static String getRoute(String url) throws URISyntaxException {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "http://" + url;
+        }
+
+        URI uri = new URI(url);
+        String path = uri.getPath();
+
+        String[] segments = path.split("/");
+
+        if (segments.length > 2) {
+            StringBuilder route = new StringBuilder();
+            for (int i = 2; i < segments.length; i++) {
+                route.append("/").append(segments[i]);
+            }
+
+            return route.toString();
+        }
+
+        return "/";
+    }
+}
