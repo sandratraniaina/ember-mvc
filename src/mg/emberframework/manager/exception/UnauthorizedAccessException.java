9a756c0018d7999796e6aad73f6b322881627535
sandratraniaina <sandratrarafa@gmail.com>
2025-02-26 08:21:49 +0300

    feat: Create UnauthorizeAccessException class

---
+++ src/mg/emberframework/manager/exception/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package mg.emberframework.manager.exception;
+
+class UnauthorizedAccessException extends Exception {
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
+}
