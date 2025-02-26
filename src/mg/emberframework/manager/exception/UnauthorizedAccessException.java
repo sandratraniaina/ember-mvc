73490c6bd14e2e407bf1323d76e408702c7d81ab
sandratraniaina <sandratrarafa@gmail.com>
2025-02-26 08:34:41 +0300

    fix: Fix UnauthorizedAccessException access modifier to public

--- src/mg/emberframework/manager/exception/UnauthorizedAccessException.java
+++ src/mg/emberframework/manager/exception/UnauthorizedAccessException.java
@@ -1,6 +1,6 @@
 package mg.emberframework.manager.exception;
 
-class UnauthorizedAccessException extends Exception {
+public class UnauthorizedAccessException extends Exception {
     public UnauthorizedAccessException(String message) {
         super(message);
     }
