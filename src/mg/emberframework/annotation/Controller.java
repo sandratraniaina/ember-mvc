cb73aa6b959c11c7d75826d4f53d753c753deb38
sandratraniaina <sandratrarafa@gmail.com>
2025-02-27 21:55:30 +0300

    feat: Add target types in annotations for better control

--- src/mg/emberframework/annotation/Controller.java
+++ src/mg/emberframework/annotation/Controller.java
@@ -1,7 +1,10 @@
 package mg.emberframework.annotation;
 
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Controller {}
