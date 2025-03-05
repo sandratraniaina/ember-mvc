c85934cd9c1f2412f52b4a2ac5375c160ecf3059
sandratraniaina <sandratrarafa@gmail.com>
2025-03-05 20:37:37 +0300

    docs: Add Java docs to all exceptions

--- src/mg/emberframework/core/exception/InvalidControllerPackageException.java
+++ src/mg/emberframework/core/exception/InvalidControllerPackageException.java
@@ -1,6 +1,20 @@
 package mg.emberframework.core.exception;
 
-public class InvalidControllerPackageException extends Exception{
+/**
+ * Thrown when the specified controller package is invalid or cannot be processed.
+ * <p>
+ * This exception is raised when the framework encounters an issue with the
+ * controller package configuration, such as an incorrect or inaccessible package name.
+ * 
+ * @author Sandratra NIAINA
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class InvalidControllerPackageException extends Exception {
+    /**
+     * Constructs an exception with a detail message.
+     * @param message the detail message explaining the invalid package
+     */
     public InvalidControllerPackageException(String message) {
         super(message);
     }
--- src/mg/emberframework/core/exception/ModelValidationException.java
+++ src/mg/emberframework/core/exception/ModelValidationException.java
@@ -1,6 +1,20 @@
 package mg.emberframework.core.exception;
 
-public class ModelValidationException extends Exception{
+/**
+ * Thrown when model validation fails.
+ * <p>
+ * This exception is raised when a model's fields do not pass the validation
+ * rules defined by annotations such as {@code @Numeric} or {@code @Required}.
+ * 
+ * @author Sandratra NIAINA
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class ModelValidationException extends Exception {
+    /**
+     * Constructs an exception with a detail message.
+     * @param message the detail message explaining the validation failure
+     */
     public ModelValidationException(String message) {
         super(message);
     }
