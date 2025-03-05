2ddb7adac10b740bed9eacce6387ca866a0a0705
sandratraniaina <sandratrarafa@gmail.com>
2025-03-05 20:28:04 +0300

    docs: Add Java docs to Numeric

--- src/mg/emberframework/annotation/validation/Numeric.java
+++ src/mg/emberframework/annotation/validation/Numeric.java
@@ -4,8 +4,18 @@ import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 
+/**
+ * Indicates that a field or parameter must contain a numeric value.
+ * <p>
+ * This annotation can be applied to fields or method parameters to enforce
+ * that their values are valid numbers (e.g., integers, decimals). The framework
+ * will validate the input accordingly.
+ * 
+ * @author Sandratra NIAINA
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.FIELD, ElementType.PARAMETER })
 public @interface Numeric {
-    
 }
