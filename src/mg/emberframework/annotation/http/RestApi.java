db242de570e2d71d00e53bf8065141e7ff4ac294
sandratraniaina <sandratrarafa@gmail.com>
2025-03-05 20:25:18 +0300

    docs: Add Java docs to RestApi

--- src/mg/emberframework/annotation/http/RestApi.java
+++ src/mg/emberframework/annotation/http/RestApi.java
@@ -5,6 +5,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that a method exposes a RESTful API endpoint.
+ * <p>
+ * Methods annotated with {@code @RestApi} are intended to handle HTTP requests
+ * and return responses in a RESTful manner in JSON format.
+ * 
+ * @author Sandratra NIAINA
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface RestApi {
