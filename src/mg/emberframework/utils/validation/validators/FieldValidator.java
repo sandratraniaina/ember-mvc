8a6a4adee5261d6d1b3145b8fa2c43587cf938a7
sandratraniaina <sandratrarafa@gmail.com>
2025-03-05 21:16:49 +0300

    feat: Deploy java docs to github pages

--- src/mg/emberframework/utils/validation/validators/FieldValidator.java
+++
@@ -1,9 +0,0 @@
-package mg.emberframework.utils.validation.validators;
-
-import java.lang.annotation.Annotation;
-
-import mg.emberframework.core.exception.ModelValidationException;
-
-public interface FieldValidator {
-    public void validate(String value, Annotation annotation, String fieldName) throws ModelValidationException;
-}
